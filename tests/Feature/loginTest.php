9df04f319335f7fdcd20b36d3698c3efe6f37361
Glazelf <gideon.steenbergen@gmail.com>
2022-12-18 23:14:33 +0100

    basic login tests

--- tests/Feature/loginTest.php
+++ tests/Feature/loginTest.php
@@ -10,12 +10,26 @@ class loginTests extends TestCase
     /** @test */
     public function loginHappyPathTest()
     {
-        $this->assertTrue(true); // TODO: Implement this test
+        {
+            $response = $this->json('POST',
+            '/login', [
+                'email' => 'test@gmail.com',
+                'password' => 't3$stP@ssw0rd'
+                ]
+            );
+            $response->assertSessionHasNoErrors(['email', 'password']);
+        }
     }
 
     /** @test */
     public function loginUnhappyPathTest()
     {
-        $this->assertTrue(true); // TODO: Implement this test
+        $response = $this->json('POST',
+        '/login', [
+            'email' => 'test@gmail.com',
+            'password' => 'Wr0NgP@ssw0rd'
+            ]
+        );
+        $response->assertSessionHasErrors(['email', 'password']);
     }
 }
